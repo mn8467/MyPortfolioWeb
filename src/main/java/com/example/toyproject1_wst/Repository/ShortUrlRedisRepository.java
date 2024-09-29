d3bfa5af61315e13cc17f971f24590fd3e65b635
mn8467 <mn8467@naver.com>
2024-09-29 21:38:37 +0900

    reflash

--- src/main/java/com/example/toyproject1_wst/Repository/ShortUrlRedisRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.toyproject1_wst.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ShortUrlRedisRepository extends CrudRepository<ShortUrlRedisRepository,String> {
-}
