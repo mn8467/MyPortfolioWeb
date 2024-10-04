503d8d48a33f1a05732a30d446348b1ae1df519b
mn8467 <mn8467@naver.com>
2024-10-04 09:59:47 +0900

    board

--- src/main/java/com/example/toyproject1_wst/Controller/BoardController.java
+++ src/main/java/com/example/toyproject1_wst/Controller/BoardController.java
@@ -1,4 +1,16 @@
 package com.example.toyproject1_wst.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
 public class BoardController {
+
+    @GetMapping("/board") //게시판 컨트롤러
+    public String boardPage (){
+        return "board";
+    }
+
 }
